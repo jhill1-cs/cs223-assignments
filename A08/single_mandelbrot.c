d4b9864981361ba489c66a4f9da7f4b8b03f1a12
Jonathan P Hill <jhill1@goldengate.cs.brynmawr.edu>
2023-03-22 21:02:12 -0400

    A08 done!

--- A08/single_mandelbrot.c
+++ A08/single_mandelbrot.c
@@ -1,3 +1,8 @@
+Name: Jonathan Hill
+Date: Mar. 22, 2023
+Description: single_mandelbrot.c computes a program that
+ outputs a PPM image of the mandelbrot set
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -30,9 +35,66 @@ int main(int argc, char* argv[]) {
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
   // todo: your work here
+  struct ppm_pixel* picture;
+  picture = (struct ppm_pixel*)malloc(size * size * sizeof(struct ppm_pixel));
+  // for (int i = 0; i < size; i++) {
+  //   picture[i] = (struct ppm_pixel*)malloc(size * sizeof(struct ppm_pixel));
+  // }
+
   // generate pallet
   srand(time(0));
+  struct ppm_pixel* color_palette;
+  color_palette = (struct ppm_pixel*)malloc((maxIterations) * sizeof(struct ppm_pixel));
+
+  for (int i = 0; i < maxIterations; i++) {
+     color_palette[i].red = rand() % 256;
+     color_palette[i].green = rand() % 256;
+     color_palette[i].blue = rand() % 256;
+  }
+
+
+  float timer;
+  struct timeval tstart, tend;
+  gettimeofday(&tstart, NULL);
+
+
 
   // compute image
+  for (int i = 0; i < size; i++){
+    for (int j = 0; j < size; j++){
+      float xfrac = (float) j / size;
+      float yfrac = (float) i / size;
+      float x0 = xmin + xfrac * (xmax - xmin);
+      float y0 = ymin + yfrac * (ymax - ymin);
+
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+      while (iter < maxIterations && (x*x + y*y < 2*2)){
+        float x_temp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = x_temp;
+        iter++;
+      }
+      if (iter < maxIterations){
+        picture[(i * size) + j] = color_palette[iter];
+      }
+      else{
+         picture[(i * size) + j].red = 0;
+         picture[(i * size) + j].blue = 0;
+         picture[(i * size) + j].green = 0;
+      }
+    }
+  }
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%ix%i) in %f seconds\n", size, size, timer);
+  char file_name[160];
+  sprintf(file_name, "mandelbrot-%i-%li.ppm" , size, time(0)); 
+  write_ppm(file_name, picture , size, size);
+  free(picture);
+  free(color_palette);
 
 }
+
+
--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,35 @@
+Name: Jonathan Hill
+Date: Mar. 22, 2023
+Description: Warmup.c spawns algorithm processes based on
+the image given to us in the assignment description.
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main() {
+   pid_t pid;
+   int child_status;
+
+   printf("%d] A0\n", getpid());
+   pid = fork();
+
+   if (pid == 0){
+        printf("%d] B1\n", getpid());
+        pid = wait(&child_status);
+        sleep(5);
+     }
+   else{
+        printf("%d] B0\n", getpid());
+        pid = wait(&child_status);
+        printf("%d] C0\n" , getpid());
+        pid = fork();
+   }
+    if (pid == 0){
+	printf("%d] C1\n", getpid());
+    }
+  printf("%d] Bye\n", getpid());
   return 0;
 }
